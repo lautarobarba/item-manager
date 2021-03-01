b6e2c693b9fc587645e2cca438fc6d03fe98ed3e
Lautaro Barba <lau_mb94@hotmail.com>
2021-03-01 00:00:13 -0300

    Se agrego proyecto a los items

--- src/app/models/item.py
+++ src/app/models/item.py
@@ -1,15 +1,17 @@
 from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
+from .proyecto import Proyecto
 from .tipo_item import TipoItem
 from .estado_item import EstadoItem
 
 class Item(models.Model):
+    proyecto = models.ForeignKey(Proyecto, verbose_name='proyecto', on_delete=models.PROTECT)
     nombre = models.CharField(verbose_name='nombre', max_length=255, unique=True)
     detalle = models.CharField(verbose_name='detalle', max_length=255)
     creacion = models.DateTimeField(verbose_name='fecha de creacion', auto_now_add=True, max_length=255)
     tipo = models.ForeignKey(TipoItem, verbose_name='tipo', on_delete=models.PROTECT)
-    estado = models.ForeignKey(EstadoItem, verbose_name='estado', default=1 ,on_delete=models.PROTECT)
+    estado = models.ForeignKey(EstadoItem, verbose_name='estado', default=1, on_delete=models.PROTECT)
     responsable = models.ForeignKey(User, verbose_name='responsable',  related_name="responsables", on_delete=models.PROTECT)
     equipo = models.ManyToManyField(User, verbose_name='equipo')
 
